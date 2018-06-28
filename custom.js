90aac0049299171a6add3b12ad441307a1702655
Buddy Moreno <buddymoreno@rocketmail.com>
2018-06-28 00:05:17 -0700

    Updated files

--- custom.js
+++
@@ -1,9 +0,0 @@
-/*  For mobile hamburger Menu */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
